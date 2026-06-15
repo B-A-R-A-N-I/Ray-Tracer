a4ee7c6cf0b3e824981b58198dab09fea05193bf
Barani <vbkumar617@gmail.com>
2026-06-15 19:54:57 +0530

    added a sphere

--- src/inOneWeekend/main.cpp
+++ src/inOneWeekend/main.cpp
@@ -3,11 +3,26 @@
 #include "vec3.h"
 #include <iostream>
 
+bool hit_sphere(const point3& center, double radius, const ray& r)
+{
+    vec3 oc = center - r.origin();
+    auto a = dot(r.direction(), r.direction());
+    auto b = -2.0 * dot(r.direction(), oc);
+    auto c = dot(oc, oc) - radius * radius;
+    auto discriminant = b*b - 4*a*c;
+    return (discriminant >= 0);
+}
+
 color ray_color(const ray& r)
 {
+    if(hit_sphere(point3(0,0,-1), 0.5, r))
+    {
+        return color(1.0, 0.83, 0.46);
+    }
+
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5 * (unit_direction.y() + 1.0);
-    return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 0.3);
+    return (1.0-a)*color(1.0, 0.8, 0.8) + a*color(1.0, 0.3, 0.3);
 }
 
 int main()
