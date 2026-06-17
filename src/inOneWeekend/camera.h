3c6abfd6208ef8c2e234012026cd5509a3f464be
Barani <vbkumar617@gmail.com>
2026-06-17 13:36:13 +0530

    added diffuse

--- src/inOneWeekend/camera.h
+++ src/inOneWeekend/camera.h
@@ -66,12 +66,13 @@ private:
         
         if(world.hit(r, interval(0, infinity), rec))
         {
-            return 0.5 * (rec.normal + color(1,1,1));
+            vec3 direction = random_on_hemisphere(rec.normal);
+            return 0.5 * ray_color(ray(rec.p, direction), world);
         }
 
         vec3 unit_direction = unit_vector(r.direction());
         auto a = 0.5 * (unit_direction.y() + 1.0);
-        return (1.0-a)*color(1.0, 0.8, 0.8) + a*color(1.0, 0.3, 0.3);
+        return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);   // Gradient for Background
     }
 
 public:
